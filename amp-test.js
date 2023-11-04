ae46be2325cfc7d85a1773ee31b9b284ceedc46a
jillianlmeyer <149703601+jillianlmeyer@users.noreply.github.com>
2023-11-04 12:32:35 -0400

    Update amp-test.js

--- amp-test.js
+++ amp-test.js
@@ -84,6 +84,15 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/amp/qualt
 
 		trialsInBlock : [40, 40, 40, 40], //Number of trials in each block 
 		trialsInExample : 5, //Change to 0 if you don't want an example block
+
+		//Duration parameters.
+		fixationDuration : 1000, //It means that by default we do not use fixation.
+		primeDuration : 100000, 
+		postPrimeDuration : 100000, //Duration of blank screen between prime and target.
+		targetDuration : 1000000, //Duration of target presentation.
+		showRatingDuration : 300, //In the 7-responses option, for how long to show the selected rating.
+		ITI : 250, //Duration between trials.
+
 		
 		sortingLabel1 : 'Neutral', //Response is coded as 0. 
 		sortingLabel2 : 'Negative',  //Response is coded as 1.
