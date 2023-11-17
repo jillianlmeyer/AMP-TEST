117e43e91367dae4a882aa9cee5bf44560e1a2cf
jillianlmeyer <149703601+jillianlmeyer@users.noreply.github.com>
2023-11-16 23:33:46 -0500

    Update amp-test.js

--- amp-test.js
+++ amp-test.js
@@ -173,7 +173,7 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/amp/qualt
 
 
 		// *********  EXAMPLE ROUND  *****************
-		examplePrimeStimulus : {
+		examplePrimeStimulus : {{color:'#FFFF00','font-size':'2.3em'},
 			nameForLogging : 'examplePrime', //Will be used in the logging
 			//An array of all media objects for this category.
 			mediaArray : [
@@ -185,7 +185,7 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/amp/qualt
 			]
 		},
 
-		exampleTargetStimulus : {
+		exampleTargetStimulus : {{color:'#0000FF','font-size':'2.3em'},
 			nameForLogging : 'exampleTarget', //Will be used in the logging
 			//An array of all media objects for this category.
 			mediaArray : [
