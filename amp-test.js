e2a0a30ca3e4b8a3e51a47c8c15006b8e7a784a7
jillianlmeyer <149703601+jillianlmeyer@users.noreply.github.com>
2023-11-22 20:43:21 -0500

    Update amp-test.js

--- amp-test.js
+++ amp-test.js
@@ -11,27 +11,21 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/amp/qualt
 		sortingLabel1 : 'Morally Wrong', //Response is coded as 0. 
 		sortingLabel2 : 'Neutral',  //Response is coded as 1.
 
-		//The default font color of text in the task (e.g., for key labels).
-			fontColor : '#FFFFFF', 
-			sortingLabel1Color : '#FFFFFF',
-			sortingLabel2Color :'#FFFFFF',
-
 		rightKey : 'm', 
 		leftKey : 'z',
 
 		targetCat : 'SECOND word', //The name of the targets (used in the instructions)
 
-		
-		//The fixation stimulus 
-			fixationStimulus : {
-				css : {color:'#FFFFFF', 'font-size':'3em'}, 
-				media : {word:'+'}
-			}, 
-			//The fixation stimulus in the example block
-			exampleFixationStimulus : {
-				css : {color:'FFFFFF', 'font-size':'3em'}, 
-				media : {word:'+'}
-			}, 
+		//Instructions text for the 2-responses version.
+			exampleBlockInst: '<div><p style="font-size:20px; text-align:left; margin-left:10px; font-family:arial"><color="000000"><br/>' + 
+			'Press the key <B>rightKey</B> if the targetCat is more rightAttribute than average. ' + 
+			'Hit the <b>leftKey</b> key if it is more leftAttribute than average.<br/><br/>' + 
+			'The items appear and disappear quickly.  ' + 
+			'Remember to ignore the first word that appears before the targetCat and evaluate only the targetCat.<br/><br/></p>'  + 
+			'<p style="font-size:16px; text-align:center; font-family:arial"><color="000000"><br/><br/>' + 
+			'When you are ready to try a few practice responses, hit the <b>space bar</b>.</p>' + 
+			'<p style="font-size:12px; text-align:center; font-family:arial">' + 
+			'<color="000000">[Round 1 of nBlocks]</p></div>',
 		
 		//  ***** REAL TRIAL ************//
 		//The CSS for all the prime stimuli.
@@ -63,14 +57,14 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/amp/qualt
 				mediaArray : [
 				    	{word : 'writing'}, 
 					{word : 'farming'},
-					{word : 'painting'},
-					{word : 'baking'},
-					{word : 'poetry'},
 					{word : 'wondering'},
-					{word : 'golf'},
-					{word : 'leisure'},
-					{word : 'modesty'},
-					{word : 'agreement'}
+					{word : 'whistling'},
+					{word : 'industry'},
+					{word : 'reunion'},
+					{word : 'nursing'},
+					{word : 'listening'},
+					{word : 'passage'},
+					{word : 'watching'}
 				]
 			}
 		],
@@ -80,16 +74,16 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/amp/qualt
 				nameForLogging : 'NeutralsT',  //Will be used in the logging
 				//An array of all media objects for this category. The default is pic1-pic200.jpg
 				mediaArray : [
-					{word : 'travel'}, 
+					{word : 'writing'}, 
+					{word : 'farming'},
+					{word : 'wondering'},
 					{word : 'whistling'},
 					{word : 'industry'},
 					{word : 'reunion'},
 					{word : 'nursing'},
 					{word : 'listening'},
 					{word : 'passage'},
-					{word : 'watching'},
-					{word : 'tennis'},
-					{word : 'exercise'}
+					{word : 'watching'}
 				]
 				
 			},
@@ -113,96 +107,46 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/amp/qualt
 
 		//Duration parameters.
 		fixationDuration : 200, //It means that by default we do not use fixation.
-		primeDuration : 200, 
+		primeDuration : 100, 
 		postPrimeDuration : 75, //Duration of blank screen between prime and target.
-		targetDuration : 200, //Duration of target presentation.
+		targetDuration : 500, //Duration of target presentation.
 		ITI : 250, //Duration between trials.
 
 		//  ********* END REAL TRIALS **********//
 
 
 		// *********  EXAMPLE ROUND  *****************
-			//Instructions text for the 2-responses version.
-			exampleBlockInst: '<div><p style="font-size:20px; text-align:left; margin-left:10px; font-family:arial"><color="FFFFFF"><br/>' + 
-			'Press the key <B>rightKey</B> if the targetCat is more rightAttribute than average. ' + 
-			'Hit the <b>leftKey</b> key if it is more leftAttribute than average.<br/><br/>' + 
-			'The items appear and disappear quickly.  ' + 
-			'Remember to ignore the item that appears before the targetCat and evaluate only the targetCat.<br/><br/></p>'  + 
-			'<p style="font-size:16px; text-align:center; font-family:arial"><color="FFFFFF"><br/><br/>' + 
-			'When you are ready to try a few practice responses, hit the <b>space bar</b>.</p>' + 
-			'<p style="font-size:12px; text-align:center; font-family:arial">' + 
-			'<color="FFFFFF">[Round 1 of nBlocks]</p></div>',
-			firstBlockInst : '<div><p style="font-size:20px; text-align:left; margin-left:10px; font-family:arial"><color="FFFFFF"><br/>' + 
-			"See how fast it is? Don't worry if you miss some. " + 
-			'Go with your gut feelings.<br/><br/>' + 
-			'Concentrate on each targetCat and rate it as more rightAttribute than the average targetCat with the <b>rightKey</b> key, ' + 
-			'or more leftAttribute than average with the <b>leftKey</b> key.<br/><br/>' + 
-			'Evaluate each targetCat and not the item that appears before it. ' + 
-			'Those items are sometimes distracting.<br/><br/>' + 
-			'<p style="font-size:16px; text-align:center; font-family:arial"><color="FFFFFF"><br/><br/>' + 
-			'Ready? Hit the <b>space bar</b>.</p>' + 
-			'<p style="font-size:12px; text-align:center; font-family:arial">' + 
-			'<color="FFFFFF">[Round 2 of nBlocks]</p></div>',
-			middleBlockInst : '<div><p style="font-size:20px; text-align:left; margin-left:10px; font-family:arial"><color="FFFFFF"><br/>' + 
-			'Continue to another round of this task. ' + 
-			'The rules are exactly the same:<br/><br/>' + 
-			'Concentrate on the targetCat and rate it as more rightAttribute than average with the <b>rightKey</b> key, ' + 
-			'or more leftAttribute than average with the <b>leftKey</b> key.<br/><br/>' + 
-			'Evaluate each targetCat and not the item that appears before it. ' + 
-			'Those items are sometimes distracting. Go with your gut feelings.<br/><br/>' + 
-			'<p style="font-size:16px; text-align:center; font-family:arial"><color="FFFFFF"><br/><br/>' + 
-			'Ready? Hit the <b>space bar</b>.</p>' + 
-			'<p style="font-size:12px; text-align:center; font-family:arial">' + 
-			'<color="FFFFFF">[Round blockNum of nBlocks]</p></div>',
-			lastBlockInst : '<div><p style="font-size:20px; text-align:left; margin-left:10px; font-family:arial"><color="FFFFFF"><br/>' + 
-			'Ready for the FINAL round? ' + 
-			'The rules are exactly the same:<br/><br/>' + 
-			'Concentrate on the targetCat and rate it as more rightAttribute than average with the <b>rightKey</b> key, ' + 
-			'or more leftAttribute than average with the <b>leftKey</b> key.<br/><br/>' + 
-			'Evaluate each targetCat and not the item that appears before it. ' + 
-			'Those items are sometimes distracting. Go with your gut feelings.<br/><br/>' + 
-			'<p style="font-size:16px; text-align:center; font-family:arial"><color="FFFFFF"><br/><br/>' + 
-			'Ready? Hit the <b>space bar</b>.</p>' + 
-			'<p style="font-size:12px; text-align:center; font-family:arial">' + 
-			'<color="FFFFFF">[Round blockNum of nBlocks]</p></div>',
-		
-		//Set the canvas of the task
-			canvas : {
-				maxWidth: 850,
-				proportions : 0.7,
-				background: '#000000',
-				borderWidth: 5,
-				canvasBackground: '#000000',
-				borderColor: '#000000'
-			},
-
-		examplePrimeStimulus : {color:'#FFFF00','font-size':'2.3em'},
-		nameForLogging : 'examplePrime', //Will be used in the logging
+		exampleprimeStimulusCSS : {color:'#FFFF00','font-size':'2.3em'},
+		examplePrimeStimulus : {
+			nameForLogging : 'examplePrime', //Will be used in the logging
 			//An array of all media objects for this category.
-		mediaArray : [
-			{word : 'painting'}, 
-			{word : 'baking'}, 
-			{word : 'poetry'},
-			{word: 'cruelty'},
-			{word: 'genocide'}
-			],
+			mediaArray : [
+				{word : 'painting'}, 
+				{word : 'baking'}, 
+				{word : 'poetry'},
+				{word: 'cruelty'},
+				{word: 'genocide'}
+			]
+		},
 
-		exampleTargetStimulus : {color:'#0000FF','font-size':'2.3em'},
-		nameForLogging : 'exampleTarget', //Will be used in the logging
+		exampleTargetStimulusCSS : {color:'#0000FF','font-size':'2.3em'},
+		exampleTargetStimulus : {
+			nameForLogging : 'exampleTarget', //Will be used in the logging
 			//An array of all media objects for this category.
-		mediaArray : [
-			{word : 'painting'}, 
-			{word : 'baking'},
-			{word : 'poetry'},
-			{word: 'cruelty'},
-			{word: 'racism'}
-			],
+			mediaArray : [
+				{word : 'painting'}, 
+				{word : 'baking'},
+				{word : 'poetry'},
+				{word: 'cruelty'},
+				{word: 'racism'}
+			]
+		},
 
 		//For the example block (often practice)  - MEASURED IN MILLISECONS.  1000MS = 1 SECOND
 		exampleBlock_fixationDuration : 200, 
-		exampleBlock_primeDuration : 200, 
+		exampleBlock_primeDuration : 100, 
 		exampleBlock_postPrimeDuration : 75, 
-		exampleBlock_targetDuration : 200,
+		exampleBlock_targetDuration : 500,
 
 		// ****** END EXAMPLE ROUND ********
 		
